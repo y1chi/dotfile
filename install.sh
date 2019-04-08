2b4c8e7656e0eaba00fcd4fa2ac1cbd4d790ba56
Yichi Zhang <zhangyichi@uber.com>
2019-04-07 22:43:34 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#install from home directory
+ln -sv "dotfile/tmux.conf" ~/.tmux.conf
+ln -sv "dotfile/vimrc.before.fork" ~/.vimrc.before.fork
+ln -sv "dotfile/vimrc.bundles.local" ~/.vimrc.bundles.local
+ln -sv "dotfile/vimrc.local" ~/.vimrc.local
