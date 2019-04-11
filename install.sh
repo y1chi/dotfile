ae8d3af1aa2f1844acad4e415ba021042ae266f5
Yichi Zhang <zhangyichi@uber.com>
2019-04-10 22:59:32 -0700

    Remove other vimrc files in spf13

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #install from home directory
 ln -sv "dotfile/tmux.conf" ~/.tmux.conf
+ln -sv "dotfile/vimrc" ~/.vimrc
 ln -sv "dotfile/vimrc.before.fork" ~/.vimrc.before.fork
 ln -sv "dotfile/vimrc.bundles.local" ~/.vimrc.bundles.local
 ln -sv "dotfile/vimrc.local" ~/.vimrc.local
